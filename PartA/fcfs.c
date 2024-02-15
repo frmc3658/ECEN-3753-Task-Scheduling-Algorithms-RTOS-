629e4fbffd01a6117b27e2f369a409bad4135925
frmc3658 <frmc3658@colorado.edu>
2024-02-14 17:25:34 -0700

    Implemented queue.c and fcfs.c

--- PartA/fcfs.c
+++ PartA/fcfs.c
@@ -2,22 +2,77 @@
 #include "queue.h"
 #include <stdio.h>
 
-void init(struct task_t *task, int *execution, int size) {
 
+void init(struct task_t* task, int* execution, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        task[i].process_id  = i;
+        task[i].execution_time = execution[i];
+    }
 }
 
-void first_come_first_served(struct task_t *task, int size) {
-    // Hints:
-    // 1. Create Queue based on the task array
-    // 2. Pop off the front item until there are no more tasks
+
+void first_come_first_served(struct task_t* task, int size)
+{
+    int runTime = 0;
+
+    // Create queue based on the task array
+    struct node_t* queue = create_queue(task, size);
+    struct node_t* head = queue->next;
+
+    // Pop off the front item until there are no more tasks
+    for(int i = 0; i < size; i++)
+    {
+        // "Execute" the first task
+        struct task_t* currentTask = peek(&head);
+
+        // Pop the head from the queue
+        pop(&head);
+
+        // Calculate task wait time and turnaround time
+        currentTask->waiting_time = runTime;
+        currentTask->turnaround_time = runTime + currentTask->execution_time;
+        
+        // Update runtime
+        runTime += currentTask->execution_time;
+
+        // Print times to console
+        printf("Task[%d] Wait Time: %d\n", i, currentTask->waiting_time);
+        printf("Task[%d] Turnaround Time: %d\n", i, currentTask->turnaround_time);
+    }
+
+    // Calculate average times
+    float avgWaitTime = calculate_average_wait_time(task, size);
+    float avgTurnaroundTime = calculate_average_turn_around_time(task, size);
+
+    // Print average times
+    printf("Average Wait Time: %f\n", avgWaitTime);
+    printf("Average Turnaround Time: %f\n", avgTurnaroundTime);
 }
 
-float calculate_average_wait_time(struct task_t *task, int size) {
-    // return 0.0 so it compiles
-    return 0.0;
+
+float calculate_average_wait_time(struct task_t* task, int size)
+{
+    int totalTime = 0;
+
+    for(int i = 0; i < size; i++)
+    {
+        totalTime += task[i].waiting_time;
+    }
+    
+    return totalTime / size;
 }
 
-float calculate_average_turn_around_time(struct task_t *task, int size) {
-    // return 0.0 so it compiles
-    return 0.0;
+
+float calculate_average_turn_around_time(struct task_t* task, int size)
+{
+    int totalTime = 0;
+
+    for(int i = 0; i < size; i++)
+    {
+        totalTime += task[i].turnaround_time;
+    }
+    
+    return totalTime / size;
 }
--- PartA/queue.c
+++ PartA/queue.c
@@ -1,35 +1,148 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "queue.h"
 
-struct node_t* create_queue(struct task_t* task, int size) {
-    // return null so it compiles
-    return NULL;
+
+struct node_t* create_queue(struct task_t* task, int size)
+{
+    // Create the sentinel node at the base of the queue
+    struct node_t* queue = create_new_node(NULL);
+
+    // Verify that malloc didn't fail
+    if(queue == NULL)
+    {
+        fprintf(stderr, "create_queue() ERROR: Malloc failed!\n");
+        return NULL;
+    }
+
+    struct node_t* currentNode = queue;
+
+    // Create and link nodes together to form the queue
+    // NOTE: The first "true" node in the queue is linked to the sentinel
+    for(int i = 0; i < size; i++)
+    {
+        currentNode->next = create_new_node(&(task[i]));
+        currentNode = currentNode->next;
+
+        if(currentNode == NULL)
+        {
+            fprintf(stderr, "create_queue() ERROR: Couldn't create a new node!\n");
+            return NULL;
+        }
+    }
+
+    return queue;
 }
 
-struct node_t* create_new_node(struct task_t* task) {
-    // return null so it compiles
-    return NULL;
+
+struct node_t* create_new_node(struct task_t* task)
+{
+    // Dynamically allocate memory for the new node
+    struct node_t* newNode = (struct node_t*)malloc(sizeof(struct node_t));
+    
+    // Verify that malloc didn't fail
+    if(newNode == NULL)
+    {
+        fprintf(stderr, "create_queue() ERROR: Malloc failed\n");
+        return NULL;
+    }
+
+    // Initialize node data members
+    newNode->task = task;
+    newNode->next = NULL;
+
+    return newNode;
 }
 
-struct task_t* peek(struct node_t** head) {
-    // return null so it compiles
-    return NULL;
+
+struct task_t* peek(struct node_t** head)
+{
+    // Return the top-most task of the queue
+    return (*head)->task;
 }
 
-void pop(struct node_t** head) {
 
+void pop(struct node_t** head)
+{
+    // Check if the queue is empty
+    if(is_empty(head))
+    {
+        return;
+    }
+
+    // Pop the old head
+    struct node_t* oldHead = *head;
+    *head = (*head)->next;
+
+    // Free the memory allocated for the popped head
+    free(oldHead);
 }
 
-void push(struct node_t** head, struct task_t* task) {
 
+void push(struct node_t** head, struct task_t* task)
+{
+    // Verify that the queue is initialized properly
+    if(head == NULL)
+    {
+        fprintf(stderr, "push() ERROR: Queue is uninitialized!\n");
+        return;
+    }
+
+    // Traverse to find the end of the queue
+    struct node_t* currentNode = *head;
+    while((currentNode != NULL) && (currentNode->next != NULL))
+    {
+        currentNode = currentNode->next;
+    }
+
+    // Create a new node
+    struct node_t* newNode = create_new_node(task);
+
+    // Verify that the newNode was created properly
+    if(newNode == NULL)
+    {
+        fprintf(stderr, "push() ERROR: Couldn't create a new node!\n");
+        return;
+    }
+
+    // Append the node
+    if(currentNode == NULL)
+    {
+        // queue was empty, append to the head
+        *head = newNode;
+    }
+    else
+    {
+        // Append to the end of the queue
+        currentNode->next = newNode;
+    }
 }
 
-int is_empty(struct node_t** head) {
-    // return 0 so it compiles
-    return 0;
+
+int is_empty(struct node_t** head)
+{
+    // Verify the first pointer is valid before trying to
+    // dereference the second pointer (which would be UB)
+    return ((head == NULL) || (*head == NULL));
 }
 
-void empty_queue(struct node_t** head) {
 
+void empty_queue(struct node_t** head)
+{
+    // Check if queue is empty or uninitialized
+    if(is_empty(head))
+    {
+        return;
+    }
+
+    struct node_t* currentNode = *head;
+    struct node_t* nextNode;
+
+    // Traverse the queue and free each node
+    while(currentNode != NULL)
+    {
+        nextNode = currentNode->next;
+        free(currentNode);
+        currentNode = nextNode;
+    }
+
+    *head = NULL;
 }
